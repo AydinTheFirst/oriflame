f6c43b1654832c802b2da03abd82714fc78eefcd
Halil Aydın <84444757+AydinTheFirst@users.noreply.github.com>
2024-01-11 21:37:00 +0300

    Delete unused assets and configuration files

--- server/src/helpers/utils.ts
+++
@@ -1,5 +0,0 @@
-import { type Response } from "express";
-
-export const APIError = (res: Response, message: string): any => {
-  return res.status(400).send({ message });
-};
--- server/src/index.ts
+++
@@ -1,9 +0,0 @@
-import "dotenv/config";
-import "./helpers/mongodb";
-// Run server
-import "./server";
-
-// Handle errors
-process.on("unhandledRejection", (reason) => {
-  console.log(reason);
-});
